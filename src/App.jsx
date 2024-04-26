b146f432e4fe314e6d268514bebde36eaaeb0aae
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-04-26 17:41:27 +0300

    HW2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Card from "./components/Card.jsx";
 
 function App() {
-    return ( <>
-    <Card/>
-    </>
-    )
-
+    return (
+        <>
+            <Card title={"Title 1"} />
+            <Card text={"Hi"} />
+            <Card title={"Title 2"} text={"how are you?"} />
+        </>
+    );
 }
 
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,22 @@
- function Card() {
-    return (
-        <div className="card">
-            <div className="card-body">
-                <h4 className="card-title">Card title</h4>
-                <p className="card-text">Some quick example text to build on the card</p>
-                <button type="button" className="btn btn-primary">Go somewhere</button>
-            </div>
-        </div>
-    )
- }
+ import React from "react";
 
+class Card extends React.Component {
+    render() {
+        const { title, text } = this.props;
+        return (
+            <div className="card">
+                <div className="card-body">
+                    {title ? <h4 className="card-title">{title}</h4> : null}
+                    {text ? <p className="card-text">{text}</p> : null}
+                </div>
+            </div>
+        );
+
+    }
+}
+
+Card.defaultProps = {
+    title: null,
+    text: null,
+};
  export default Card
